8c4cee46d4504d12a9daa7d79e2e394767f3f738
frankysu <frankysu@protonmail.com>
2019-12-03 21:50:33 +0800

    20191203

--- avatarProcess.py
+++ avatarProcess.py
@@ -9,7 +9,6 @@ from db_conn import get_db
 
 def type_convert(filename):
     pngname = '/'.join(filename.split('/')[0:-1]) + '/' + filename.split('/')[-1].split('.')[0] + '.png'
-    print(filename, pngname)
     img = cv2.imread(filename)
     cv2.imwrite(pngname, img)
 
@@ -26,4 +25,4 @@ if __name__ == "__main__":
     path = r'./testimg'
     files = [path+'/'+x for x in os.listdir(r'./testimg')]
     for file in files:
-        type_convert('./testimg/无标题.bmp')
+        type_convert(file)
